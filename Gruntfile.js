bc778b78c97493042c307dc035ff9916fcfefc11
Hiroki Kihara <fj8550jf@aa.jp.fujitsu.com>
2015-10-14 16:18:50 +0900

    add grunt gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,11 +22,24 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshintrc: {
       app: grunt.file.readJSON('.jshintrc')
-    } 
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist',
+        user: {
+          name: 'hirokihara',
+          email: 'benzoumk2@gmail.com'
+        }
+      },
+      src: [
+        '**/*',
+        '**/.*'
+      ]
+    }
   };
 
   var configs = require('load-grunt-configs')(grunt, options);
-  
+
   // Define the configuration for all the tasks
   grunt.initConfig(configs);
 
@@ -36,7 +49,7 @@ module.exports = function (grunt) {
     }
     if (target === 'dist') {
       return grunt.task.run([
-      'build', 
+      'build',
       'express:dev',
       'browserSync:dist']);
     }
@@ -127,7 +140,7 @@ module.exports = function (grunt) {
   grunt.registerTask('metric', function (target) {
     if (target === 'test') {
       return grunt.task.run([
-      'plato:test', 
+      'plato:test',
       'browserSync:metricTest'
       ]);
     }
